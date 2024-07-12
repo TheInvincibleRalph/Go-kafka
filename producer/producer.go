1f2328a7b7e40599c41a9f413be4308e62b0ce2d
TheInvincible <adesegunraphaeladeoye@gmail.com>
2024-07-12 14:58:46 +0100

    initial commit

--- producer/producer.go
+++ producer/producer.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"encoding/json"
+	"log"
+
+	"github.com/gofiber/fiber"
+)
+
 type Comment struct {
 	Text string `form:"text" json:"text"`
 }
@@ -9,4 +16,32 @@ func main() {
 	api := app.Group("/api/vi")
 	ap.Post("/comments", createComment)
 	app.Listen(":3000")
+}
+
+func createComment(c *fiber.Ctx) error {
+	cmt := new(Comment)
+	if err := c.BodyParser(cmt); err != nil {
+		log.Println(err)
+		c.Status(400).JSON(&fiber.Map{
+			"success": false,
+			"message": err,
+		})
+		return err
+	}
+	cmtInBytes, err := json.Marshal(cmt)
+	PushCommentToQueue("comments", cmtInBytes)
+
+	err = c.JSON(&fiber.Map{
+		"success": true,
+		"message": "Comment pushed successfully",
+		"comment": cmt,
+	})
+
+	if err != nil {
+		c.Status(500).JSON(&fiber.Map{
+			"success": false,
+			"message": "Error creating product",
+		})
+		return err
+	}
 }
